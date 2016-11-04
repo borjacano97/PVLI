f5b75eb58e0815be32a04d78175fe9110f6f8643
AlvaroErrepeBass <alvrod09@ucm.es>
2016-11-04 18:42:35 +0100

    Subido primer commit, resuelto entities, character e items

---
+++ src/Character.js
@@ -0,0 +1,83 @@
+'use strict';
+var dice = require('./dice');
+
+function Character(name, features) {
+  features = features || {};
+  this.name = name;
+  // Extrae del parámetro features cada característica y alamacénala en
+  // una propiedad.
+ 
+  this.initiative = features.initiative || 0;
+  this._defense = features.defense || 0;
+  this.weapon = features.weapon || null;
+  this._hp = features.hp || 0;
+  this.maxHp = features.maxHp || this._hp || 15;
+ 
+  this._mp = features.mp || 0;
+  this.maxMp = features.maxMp || this._mp;
+  this.party = null;
+}
+
+Character.prototype._immuneToEffect = ['name', 'weapon'];
+
+Character.prototype.isDead = function () {
+  // Rellena el cuerpo de esta función
+  return (this.hp < 1);
+};
+
+Character.prototype.applyEffect = function (effect, isAlly) {
+  // Implementa las reglas de aplicación de efecto para modificar las
+  // características del personaje. Recuerda devolver true o false según
+  // si el efecto se ha aplicado o no.
+  var self = this;
+
+  if (!isAlly){
+    var aleat = dice.d100();
+    if (aleat <= this.defense) return false;
+  }
+
+ /* this.features.forEach( function (effect) ){
+    self.
+  }*/
+
+  this.initiative += effect.initiative;
+  this.defense += effect.defense;
+  this.hp += effect.hp;
+  this.maxHp += effect.maxHp;
+  this.mp += effect.mp;
+  this.maxMp += effect.maxMp;
+  return true;
+};
+
+Object.defineProperty(Character.prototype, 'mp', {
+  get: function () {
+    return this._mp;
+  },
+  set: function (newValue) {
+    this._mp = Math.max(0, Math.min(newValue, this.maxMp));
+  }
+});
+
+Object.defineProperty(Character.prototype, 'hp', {
+  // Puedes usar la mísma ténica que antes para mantener el valor de hp en el
+  // rango correcto.
+   get: function () {
+    return this._hp;
+  },
+  set: function (newValue) {
+    this._hp = Math.max(0, Math.min(newValue, this.maxHp));
+  }
+});
+
+// Puedes hacer algo similar a lo anterior para mantener la defensa entre 0 y
+// 100.
+Object.defineProperty(Character.prototype, 'defense', {
+   get: function () {
+    return this._defense;
+  },
+  set: function (newValue) {
+    this._defense= Math.max(0, Math.min(newValue, 100));
+  }
+});
+
+module.exports = Character;
---
+++ src/items.js
@@ -0,0 +1,51 @@
+'use strict';
+
+function Item(name, effect) {
+  this.name = name;
+  this.effect = effect;
+}
+
+function Weapon(name, damage, extraEffect) {
+  var extraEffect = extraEffect || new Effect({});
+  extraEffect.hp += damage*-1;
+  Item.call(this, name, extraEffect);
+  
+  // Haz que Weapon sea subtipo de Item haciendo que llame al constructor de
+  // de Item.
+}
+Weapon.prototype = Object.create(Item.prototype);
+Weapon.prototype.constructor = Weapon;
+// Termina de implementar la herencia haciendo que la propiedad prototype de
+// Item sea el prototipo de Weapon.prototype y recuerda ajustar el constructor.
+
+function Scroll(name, cost, effect) {
+  this.cost = cost;
+  Item.call(this, name, effect);
+  
+}
+Scroll.prototype = Object.create(Item.prototype);
+Scroll.prototype.constructor = Scroll;
+
+Scroll.prototype.canBeUsed = function (mp) {
+  return (mp >= this.cost);
+};
+
+function Effect(variations) {
+  variations = variations || {};
+
+  this.initiative = variations.initiative || 0;
+  this.defense = variations.defense || 0;
+  this.hp = variations.hp || 0;
+  this.maxHp = variations.maxHp || 0;
+  this.mp = variations.mp || 0;
+  this.maxMp = variations.maxMp || 0;
+  // Copia las propiedades que se encuentran en variations como propiedades de
+  // este objeto.
+}
+
+module.exports = {
+  Item: Item,
+  Weapon: Weapon,
+  Scroll: Scroll,
+  Effect: Effect
+};
